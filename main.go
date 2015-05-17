273c1f3f483e9de718d991267a45c3eaf163b2fb
remybreuils <remy.breuils@gmail.com>
2015-05-17 10:27:11 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 )
 
 func main() {
+	fmt.Println("Launch http server on port 8080")
 	http.HandleFunc("/rtl/podcast/integral-les-grosses-tetes.xml", getRtlLgti)
 	http.HandleFunc("/", getRtlLgti)
 	http.ListenAndServe(":8080", nil)
-
-	fmt.Println("Launch server on 8080")
 }
 
 type rssFeed struct {
